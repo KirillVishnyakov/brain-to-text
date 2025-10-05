83b592dc7c94c2eb749b5e74f866f25302c77ba7
Kirill Vishnyakov <kir.vishn@gmail.com>
2025-10-05 18:00:10 -0400

    params

--- model_training/train_model.py
+++ model_training/train_model.py
@@ -1,9 +1,6 @@
 from omegaconf import OmegaConf
 from rnn_trainer import BrainToTextDecoder_Trainer
 
-import warnings
-
-warnings.filterwarnings("ignore")
 
 args = OmegaConf.load('rnn_args.yaml')
 trainer = BrainToTextDecoder_Trainer(args)
